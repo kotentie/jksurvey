227330f9d0b50a0505e3fb74982eb41f9d749668
Jake <jaker@Jakes-MacBook.local>
2014-03-02 12:42:30 -0600

    add rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jksurvey::Application.config.secret_key_base = '6142e45884c2e9d2187e16e8650fd834615e9c48e9e9e4e066ffada33dd2a443a6bedcf42d5c635184e3c5f421d8294daf44e5a9e4ee0c9d0f21c4f0834395df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jksurvey::Application.config.session_store :cookie_store, key: '_jksurvey_session'
