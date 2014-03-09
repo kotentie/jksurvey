98654a0c8a318accc105df8448443eb707c58ec7
Jake <jaker@Jakes-MacBook.local>
2014-03-08 18:19:06 -0600

    ad thank you erb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 	def show
 		@question = Question.find(params[:id])
-		@questions =  @question.choices		
+		@choices =  @question.choices		
 	end
 
 	def answer
